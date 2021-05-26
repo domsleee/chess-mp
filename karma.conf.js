188204feb84e5393f8823e7d6eee00de4c6b7e99
Dom Slee <domslee97@gmail.com>
2021-05-26 19:34:37 +1000

    Add chrome headless

--- karma.conf.js
+++ karma.conf.js
@@ -30,6 +30,12 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessCI: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox']
+      }
+    },
   });
 };
